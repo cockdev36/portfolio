5d6e71128a41f6bd9ba048191a7ca7cc362a0d79
cockdev <ulrichbusch2023@gmail.com>
2024-11-18 03:10:39 +0100

    Completed react tailwindcss portfolio

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'Passionate and experienced Java Software Engineer with over 5 years of professional experience in designing, developing, and maintaining high-performance Java applications. Seeking to leverage my technical expertise and problem-solving skills to contribute to a dynamic development team.',
+	},
+	{
+		id: 2,
+		bio: 'Highly skilled AI Developer with over 8 years of experience in designing, developing, and deploying machine learning models and AI solutions. Proficient in Python, TensorFlow, PyTorch, and other AI frameworks. Demonstrated expertise in natural language processing, computer vision, and deep learning. Adept at working collaboratively in dynamic environments to solve complex problems and drive innovation.',
+	},
+];
